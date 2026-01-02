8f22775ccd4752b82b2042cd96304b62b0b4fc7b
VaishnaviGanagi <vaishnavivaishu8484@gmail.com>
2026-01-02 19:43:17 +0530

    array program

---
+++ arr.py
@@ -0,0 +1,9 @@
+def main():
+    scores=[78,85,66,88,65]
+    total=sum(scores)
+    average=total/len(scores)
+    print(f"scores:{scores}")
+    print(f"sum:{total}")
+    print(f"average:{average}")
+if __name__=="__main__":
+    main()
